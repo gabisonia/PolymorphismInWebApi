9e580e0c39897f051d20e02b58b877dafb116fa8
gabisonia <irakli@yepic.ai>
2022-11-24 16:49:12 +0400

    Add logger

--- PolymorphismInWebApi/Program.cs
+++ PolymorphismInWebApi/Program.cs
@@ -1,5 +1,8 @@
 using JsonSubTypes;
 using PolymorphismInWebApi.Models;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -49,6 +52,47 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+AddLogger(builder.Logging);
+
 app.MapControllers();
 
 app.Run();
+
+
+static void AddLogger(ILoggingBuilder loggingBuilder)
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+        .Build();
+
+    var elasticEnabled = configuration.GetValue<bool>("Logging:Elastic");
+    var debugLogEnabled = configuration.GetValue<bool>("Logging:Console");
+    var dbLogEnabled = configuration.GetValue<bool>("Logging:Database");
+
+    var connectionString = configuration["ConnectionStrings:Default"];
+
+    var logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Conditional(evt => elasticEnabled, wt => wt
+            .Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearchUrl"]))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                IndexFormat = $"Application-Logs-{environment}-{DateTime.UtcNow:yyyy-MM}"
+            })
+            .Enrich.WithProperty("Environment", environment))
+        .WriteTo.Conditional(evt => debugLogEnabled, wt => wt.Console())
+        .WriteTo.Conditional(evt => dbLogEnabled, wt => wt.PostgreSQL(connectionString,
+            "Logs",
+            needAutoCreateTable: true,
+            respectCase: true,
+            restrictedToMinimumLevel: LogEventLevel.Error,
+            useCopy: false))
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog(logger);
+}
